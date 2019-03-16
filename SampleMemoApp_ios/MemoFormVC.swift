1f31a60e0e3a11ce14b4fdb0c4dc951a4e2192c0
parkjunghun <go9018@naver.com>
2019-03-16 23:25:26 +0900

    alert / memo textview custom finish

--- SampleMemoApp_ios/MemoFormVC.swift
+++ SampleMemoApp_ios/MemoFormVC.swift
@@ -27,15 +27,38 @@ class MemoFormVC: UIViewController , UIImagePickerControllerDelegate, UINavigati
         self.contents.layer.borderColor = UIColor.clear.cgColor
         self.contents.backgroundColor = UIColor.clear
         
+        //줄 간격
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 9
+        self.contents.attributedText = NSAttributedString(string: " ", attributes: [NSAttributedString.Key.paragraphStyle : style])
+        self.contents.text = ""
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let bar = self.navigationController?.navigationBar
+        
+        let ts = TimeInterval(0.3)
+        UIView.animate(withDuration: ts){
+            bar?.alpha = (bar?.alpha == 0 ? 1 : 0)
+        }
     }
     
     //저장 버튼을 클릭했을 때 호출되는 메소드
     @IBAction func save(_ sender: Any) {
+        //경고창에 사용될 컨텐츠 뷰 컨트롤러 생성
+        let alertV = UIViewController()
+        let iconImage = UIImage(named: "warning-icon-60")
+        alertV.view = UIImageView(image: iconImage)
+        alertV.preferredContentSize = iconImage?.size ?? CGSize.zero
+        
         //1.내용을 입력하지않고 저장할경우, 경고!
         guard self.contents.text?.isEmpty == false else{
             //내용이 비어있을경우
             let alert = UIAlertController(title: nil, message: "내용을 입력해주세요", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            //컨텐츠 뷰 영역에 뷰를 등록!
+            alert.setValue(alertV, forKey: "contentViewController")
             self.present(alert, animated: true)
             return
         }
